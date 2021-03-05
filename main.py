3a0d059f03f36e302752fabfbfbe36e49879027c
Zlata Russ <cassandra.from.russia@gmail.com>
2021-03-05 16:15:26 +0000

    End of Lesson 02

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
-
+MOD_SIZE = 50
 
 
 class Player(pygame.sprite.Sprite):
@@ -25,17 +25,55 @@ class Player(pygame.sprite.Sprite):
         self.step_x = step_x
         self.step_y = step_y
     
-    def update(self):
-        self.rect.y += self.step_y
-        self.rect.x += self.step_x
-        if self.rect.bottom >= HEIGHT:
-            self.step_y = -abs(self.step_y)
-        if self.rect.top <= 0:
-            self.step_y = abs(self.step_y)
-        if self.rect.right >= WIDTH:
-            self.step_x = -abs(self.step_x)      
-        if self.rect.left <= 0:
-            self.step_x = abs(self.step_x) 
+    def update (self):
+        self.speedx = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT] and self.rect.left > 0:
+            self.speedx = -8
+
+        if keystate[pygame.K_RIGHT] and self.rect.right < WIDTH:
+            self.speedx = 8
+            
+        # if self.rect.right >= WIDTH:
+        #     self.speedx = 0
+        
+        # if self.rect.left <= 0:
+        #     self.speedx = 0
+        self.rect.x += self.speedx    
+class MOD(pygame.sprite.Sprite):
+    def __init__(self, color=RED, x=WIDTH/2, y=HEIGHT/2, step_x=5, step_y=5):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((MOD_SIZE, 50))
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.rect.center = (random.randrange(0, WIDTH), -MOD_SIZE)
+        self.step_x = step_x
+        self.step_y = step_y
+    
+    def update (self):
+        self.rect.y += 5
+        if self.rect.top >= HEIGHT:
+            self.rect.y = -50
+            
+        # if self.rect.right >= WIDTH:
+        #     self.speedx = 0
+        
+        # if self.rect.left <= 0:
+        #     self.speedx = 0
+        # self.rect.x += self.speedx    
+
+
+    # def update(self):
+    #     self.rect.y += self.step_y
+    #     self.rect.x += self.step_x
+    #     if self.rect.bottom >= HEIGHT:
+    #         self.step_y = -abs(self.step_y)
+    #     if self.rect.top <= 0:
+    #         self.step_y = abs(self.step_y)
+    #     if self.rect.right >= WIDTH:
+    #         self.step_x = -abs(self.step_x)      
+    #     if self.rect.left <= 0:
+    #         self.step_x = abs(self.step_x) 
 
     # def update(self):
     #     self.rect.y += self.step_y
@@ -56,13 +94,15 @@ class Player(pygame.sprite.Sprite):
     #     if self.rect.top <= 0:
     #         self.step = 5
     
-    # def update(self):   
+    # # def update(self):   
     #     self.rect.x += self.step
     #     if self.rect.right >= WIDTH:
     #         self.step = -5
     #     if self.rect.left <= 0:
     #         self.step = 5
     
+
+
     # def update(self):
     #     self.rect.x += self.step
     #     if self.rect.right >= WIDTH:
@@ -90,14 +130,20 @@ pygame.display.set_caption("My Cats")
 clock = pygame.time.Clock()
 all_sprites = pygame.sprite.Group()
 player = Player(color=GREEN, y=HEIGHT-25)
-player2 = Player(color=BLUE, y=HEIGHT/4, step_x=-1)
-player3 = Player(RED, WIDTH/4, HEIGHT/8, step_y=-4)
-player4 = Player(RED, WIDTH/4, 3*HEIGHT, step_y=-4)
+# player2 = Player(color=BLUE, y=HEIGHT/4, step_x=-1)
+# player3 = Player(RED, WIDTH/4, HEIGHT/8, step_y=-4)
+# player4 = Player(RED, WIDTH/4, 3*HEIGHT, step_y=-4)
 all_sprites.add(player)
-all_sprites.add(player2)
-all_sprites.add(player3)
-all_sprites.add(player4)
-
+# all_sprites.add(player2)
+# all_sprites.add(player3)
+# all_sprites.add(player4)
+mod = MOD()
+all_sprites.add(mod)
+mods = pygame.sprite.Group()
+for i in range(8):
+    m = MOD()
+    all_sprites.add(m)
+    mods.add(m)
 # Цикл игры
 running = True
 while running:
@@ -111,6 +157,7 @@ while running:
     
     # Обновление
     all_sprites.update()
+
 
     # Рендеринг
     screen.fill(BLACK)
